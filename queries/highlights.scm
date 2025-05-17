6901bebe48c224c6f76324f149774eeab8ee2fec
Georgii Plotnikov <accembler@gmail.com>
2025-05-17 19:39:40 +0900

    Refactor highlights syntax for improved readability; update comments and consolidate punctuation, operators, keywords, and types

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,77 +1,72 @@
-;; comments
+;; Comments
 (comment) @comment
 
-;; punctuation
-[  ";" "," "{" "}" "(" ")" "[" "]" ] @punctuation
+;; Punctuation symbols
+[ ";" ":" "," "{" "}" "(" ")" "[" "]" "<" ">" ] @punctuation
 
-;; operators
-[ "=" "->" "::" "+" "-" "*" "**" "%" "==" "!=" "<" ">" "<=" ">=" "&&" "||" "&" "|" "^" "<<" ">>" ] @operator
-(">") @operator
-("<=") @operator
-(">=") @operator
-("&&") @operator
-("||") @operator
-("&") @operator
-("|") @operator
-("^") @operator
-("<<") @operator
-(">>") @operator
+[ "struct" "enum" "const" "spec" "return" "self" "loop" "let" "assume" "exists" "unique" "if" "else" "break" "use" "from" ] @keyword
 
-;; keywords
-(function_keyword) @keyword
-(forall_keyword) @keyword
-("exists") @keyword
-("assume") @keyword
-("unique") @keyword
-("loop") @keyword
-("if") @keyword
-("else") @keyword
-("break") @keyword
-("return") @keyword
-("let") @keyword
-("const") @keyword
-("type") @keyword
-("enum") @keyword
-("struct") @keyword
-("use") @keyword
-("spec") @keyword
-("external") @keyword
 
-;; boolean literals
+;; Operators (node types from grammar.js)
+(assign_operator) @operator
+(rightarrow_operator) @operator
+(expand_operator) @operator
+(attribute_access_operator) @operator
+(add_operator) @operator
+(sub_operator) @operator
+(mul_operator) @operator
+(pow_operator) @operator
+(mod_operator) @operator
+(equals_operator) @operator
+(not_equals_operator) @operator
+(less_operator) @operator
+(greater_operator) @operator
+(less_equal_operator) @operator
+(greater_equal_operator) @operator
+(and_operator) @operator
+(or_operator) @operator
+(bit_and_operator) @operator
+(bit_or_operator) @operator
+(bit_xor_operator) @operator
+(shift_left_operator) @operator
+(shift_right_operator) @operator
+(unary_not) @operator
+
+;; Keywords (token nodes)
+
+((function_keyword) @keyword)
+((forall_keyword) @keyword)
+((mut_keyword) @keyword)
+
+;; Boolean literals
 (bool_literal) @boolean
 
-;; types
-(type_i8) @type
-(type_i16) @type
-(type_i32) @type
-(type_i64) @type
-(type_u8) @type
-(type_u16) @type
-(type_u32) @type
-(type_u64) @type
-(type_bool) @type
-(type_unit) @type
+;; Types
+(type_i8)  @type  (type_i16) @type  (type_i32) @type  (type_i64) @type
+(type_u8)  @type  (type_u16) @type  (type_u32) @type  (type_u64) @type
+(type_bool) @type (type_unit) @type  (type_array) @type  (type_fn) @type
 
-;; literals
-(string_literal)        @string
-(number_literal)        @number
-(unit_literal)          @constant
+;; Literals
+(string_literal) @string
+(number_literal) @number
+(unit_literal) @constant
+(uzumaki_keyword) @constant
 
-;; identifiers
+;; Identifiers
 (identifier) @identifier
 
-;; define-captures for names
-(variable_definition_statement
-  name: (identifier) @variable)
-(constant_definition
-  name: (identifier) @constant)
-(function_definition
-  name: (identifier) @function)
-(external_function_definition
-  name: (identifier) @function)
-(type_definition_statement
-  name: (identifier) @type.definition)
-(enum_definition
-  name: (identifier) @type.definition)
-(struct_definition
-  name: (identifier) @type.definition)
+;; Name definitions
+;;(variable_definition_statement
+;;name: (identifier) @variable)
+;;(constant_definition
+;;name: (identifier) @constant)
+;;(function_definition
+;;name: (identifier) @function)
+;;(external_function_definition
+;;name: (identifier) @function)
+;;(type_definition_statement
+;;name: (identifier) @type.definition)
+;;(enum_definition
+;;name: (identifier) @type.definition)
+;;(struct_definition
+;;name: (identifier) @type.definition)
